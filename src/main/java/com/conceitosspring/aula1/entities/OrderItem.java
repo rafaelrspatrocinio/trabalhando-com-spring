d5553b2dd87bd6f494f19bd11906b47c3f2d5f92
rafaelrspatrocinio <rafael.silvapatrocinio@everis.nttdata.com>
2022-03-21 15:35:17 -0300

    Implementação da Classe OrderItem parte 1

---
+++ src/main/java/com/conceitosspring/aula1/entities/OrderItem.java
@@ -0,0 +1,66 @@
+package com.conceitosspring.aula1.entities;
+
+import com.conceitosspring.aula1.entities.pk.OrderItemPK;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_order_item")
+public class OrderItem implements Serializable {
+
+    private static long serialVersionUID = 1l;
+
+    @EmbeddedId
+    private OrderItemPK id;
+
+    private Integer quantity;
+    private Double price;
+
+    public OrderItem(Order order, Product product,Integer quantity, Double price) {
+        super();
+        id.setOrder(order);
+        id.setProduct(product);
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public Order getOrder(){
+        return id.getOrder();
+    }
+
+    public void setOrder(Order order){
+        id.setOrder(order);
+    }
+
+    public Product getProduct(){
+        return id.getProduct();
+    }
+
+    public void setProduct(Product product){
+        id.setProduct(product);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof OrderItem)) return false;
+        OrderItem orderItem = (OrderItem) o;
+        return Objects.equals(getId(), orderItem.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
---
+++ src/main/java/com/conceitosspring/aula1/entities/pk/OrderItemPK.java
@@ -0,0 +1,30 @@
+package com.conceitosspring.aula1.entities.pk;
+
+import com.conceitosspring.aula1.entities.Order;
+import com.conceitosspring.aula1.entities.Product;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@Embeddable
+public class OrderItemPK implements Serializable {
+
+    private static long serialVersionUID = 1l;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+}
